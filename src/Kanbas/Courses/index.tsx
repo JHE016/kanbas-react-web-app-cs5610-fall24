85f80699df8395515a839889c01cd317095ce228
JinjinHe <he.jinj@northeastern.edu>
2024-09-11 21:33:08 -0700

    3.7 Added assignment screen

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 import { Navigate, Route, Routes } from "react-router";
 
 export default function Courses() {
@@ -20,7 +21,7 @@ export default function Courses() {
                             <Route path="Modules" element={<Modules />} />
                             <Route path="Piazza" element={<h2>Piazza</h2>} />
                             <Route path="Zoom" element={<h2>Zoom</h2>} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="Quizzes" element={<h2>Quizzes</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
