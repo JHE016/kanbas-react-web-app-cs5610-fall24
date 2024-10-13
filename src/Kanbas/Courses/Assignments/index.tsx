49c545dcb7718312ddb26cbb48b6f0982b14fbbc
JinjinHe <he.jinj@northeastern.edu>
2024-10-12 18:58:12 -0700

    Completed part 3 Implementing data driven kanbas application

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -3,8 +3,13 @@ import AssignmentControl from "./AssignmentControl";
 import ControlButtons from "./ControlButtons";
 import { PiNotePencil } from "react-icons/pi";
 import LessonControlButtons from "../Modules/LessonControlButtons";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.assignments;
+
     return (
         <div id="wd-assignments" className="p-3">
             <AssignmentControl />
@@ -15,109 +20,37 @@ export default function Assignments() {
                 </div>
                 <ControlButtons />
             </div>
-
             <ul id="wd-assignment-list" className="list-group rounded-0">
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1 d-flex justify-content-between align-items-start">
-                    <div className="d-flex align-items-start">
-                        <BsGripVertical className="me-3 fs-3" />
-                        <PiNotePencil className="me-3 fs-4 text-success" />
-                        <div>
-                            {/* <h5 className="fw-bold mb-1">A1</h5> */}
-                            <a className="wd-assignment-link"
-                                href="#/Kanbas/Courses/1234/Assignments/123"
-                                style={{ color: "black", fontWeight: "bold", textDecoration: "none", fontSize: "20px" }}>
-                                A1
-                            </a>
-                            <div>
-                                <span className="text-danger me-2"> Multiple Modules</span> |
-                                <span className="text-dark fw-bold me-2"> Not available until</span>
-                                <span className="text-dark">May 6 at 12:00am</span> |
+                {assignments
+                    .filter((assignment: any) => assignment.course === cid)
+                    .map((assignment: any) => (
+                        <li key={assignment._id} className="wd-assignment-list-item list-group-item p-3 ps-1 d-flex justify-content-between align-items-start">
+                            <div className="d-flex align-items-start">
+                                <BsGripVertical className="me-3 fs-3" />
+                                <PiNotePencil className="me-3 fs-4 text-success" />
+                                <div>
+                                    <a className="wd-assignment-link"
+                                        href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
+                                        style={{ color: "black", fontWeight: "bold", textDecoration: "none", fontSize: "20px" }}>
+                                        {assignment.title}
+                                    </a>
+                                    <div>
+                                        <span className="text-danger me-2"> {assignment.modules}</span> |
+                                        <span className="text-dark fw-bold me-2"> Not available until</span>
+                                        <span className="text-dark">{assignment.notAvailableUntil}</span> |
+                                    </div>
+                                    <div className="text-dark">
+                                        <span className="fw-bold text-dark me-2">Due</span>
+                                        {assignment.due} | {assignment.score}
+                                    </div>
+                                </div>
                             </div>
-                            <div className="text-dark">
-                                <span className="fw-bold text-dark me-2">Due</span>
-                                May 13 at 11:59pm | 100 pts
+                            <div className="d-flex align-items-center">
+                                <LessonControlButtons />
                             </div>
-                        </div>
-                    </div>
-                    <div className="d-flex align-items-center">
-                        <LessonControlButtons />
-                    </div>
-                </li>
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1 d-flex justify-content-between align-items-start">
-                    <div className="d-flex align-items-start">
-                        <BsGripVertical className="me-3 fs-3" />
-                        <PiNotePencil className="me-3 fs-4 text-success" />
-                        <div>
-                            <a className="wd-assignment-link"
-                                href="#/Kanbas/Courses/1234/Assignments/123"
-                                style={{ color: "black", fontWeight: "bold", textDecoration: "none", fontSize: "20px" }}>
-                                A2
-                            </a>
-                            <div>
-                                <span className="text-danger me-2"> Multiple Modules</span> |
-                                <span className="text-dark fw-bold me-2"> Not available until</span>
-                                <span className="text-dark">June 6 at 12:00am</span> |
-                            </div>
-                            <div className="text-dark">
-                                <span className="fw-bold text-dark me-2">Due</span>
-                                June 13 at 11:59pm | 100 pts
-                            </div>
-                        </div>
-                    </div>
-                    <div className="d-flex align-items-center">
-                        <LessonControlButtons />
-                    </div>
-                </li>
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1 d-flex justify-content-between align-items-start">
-                    <div className="d-flex align-items-start">
-                        <BsGripVertical className="me-3 fs-3" />
-                        <PiNotePencil className="me-3 fs-4 text-success" />
-                        <div>
-                            <a className="wd-assignment-link"
-                                href="#/Kanbas/Courses/1234/Assignments/123"
-                                style={{ color: "black", fontWeight: "bold", textDecoration: "none", fontSize: "20px" }}>
-                                A3
-                            </a>
-                            <div>
-                                <span className="text-danger me-2"> Multiple Modules</span> |
-                                <span className="text-dark fw-bold me-2"> Not available until</span>
-                                <span className="text-dark">July 6 at 12:00am</span> |
-                            </div>
-                            <div className="text-dark">
-                                <span className="fw-bold text-dark me-2">Due</span>
-                                July 13 at 11:59pm | 100 pts
-                            </div>
-                        </div>
-                    </div>
-                    <div className="d-flex align-items-center">
-                        <LessonControlButtons />
-                    </div>
-                </li>
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1 d-flex justify-content-between align-items-start">
-                    <div className="d-flex align-items-start">
-                        <BsGripVertical className="me-3 fs-3" />
-                        <PiNotePencil className="me-3 fs-4 text-success" />
-                        <div>
-                            <a className="wd-assignment-link"
-                                href="#/Kanbas/Courses/1234/Assignments/123"
-                                style={{ color: "black", fontWeight: "bold", textDecoration: "none", fontSize: "20px" }}>
-                                A4
-                            </a>
-                            <div>
-                                <span className="text-danger me-2"> Multiple Modules</span> |
-                                <span className="text-dark fw-bold me-2"> Not available until</span>
-                                <span className="text-dark">August 6 at 12:00am</span> |
-                            </div>
-                            <div className="text-dark">
-                                <span className="fw-bold text-dark me-2">Due</span>
-                                August 13 at 11:59pm | 100 pts
-                            </div>
-                        </div>
-                    </div>
-                    <div className="d-flex align-items-center">
-                        <LessonControlButtons />
-                    </div>
-                </li>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     );
--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,179 +1,31 @@
 import { Link } from "react-router-dom";
+import * as db from "../Database";
 
 export default function Dashboard() {
+    const courses = db.courses;
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-            <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
+            <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses" className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course1.jpg" width="100%" alt="course1" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5100 Full Stack Software Developement
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Full Stack software developer
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
+                    {courses.map((course) => (
+                        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+                            <div className="card rounded-3 overflow-hidden">
+                                <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                                    className="wd-dashboard-course-link text-decoration-none text-dark" >
+                                    <img src={course.image} width="100%" height={160} />
+                                    <div className="card-body">
+                                        <h5 className="wd-dashboard-course-title card-title">
+                                            {course.name} </h5>
+                                        <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                                            {course.description} </p>
+                                        <button className="btn btn-primary"> Go </button>
+                                    </div>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course2.jpg" width="100%" alt="course2" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5200 Database Management
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Database Management
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course3.jpg" width="100%" alt="course3" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5300 Computer Science Foundations
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Computer Science Foundations
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course4.jpg" width="100%" alt="course4" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5400 Discrete Structures
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Discrete Structures
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course5.jpg" width="100%" alt="course5" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5500 Object-oriented Design
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Object-oriented Design
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course6.jpg" width="100%" alt="course6" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5600 Computer Systems
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Computer Systems
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course7.jpg" width="100%" alt="course7" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5700 Introduction of Machine Learning
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Machine Learning
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-
-                    <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                        <div className="card rounded-3 overflow-hidden">
-                            <Link className="wd-dashboard-course-link text-decoration-none text-dark"
-                                to="/Kanbas/Courses/1234/Home">
-                                <img src="/images/courses/course8.jpg" width="100%" alt="course8" height={160} />
-                                <div className="card-body">
-                                    <h5 className="wd-dashboard-course-title card-title">
-                                        CS5800 Data Structure and Algorithms
-                                    </h5>
-                                    <p className="wd-dashboard-course-title card-text">
-                                        Algorithms
-                                    </p>
-                                    <p>
-                                        2024Fall Semester Full-Term
-                                    </p>
-                                    <button className="btn btn-primary"> Go </button>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
